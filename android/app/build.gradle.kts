8ce9689b3e7a8e885333b6fe4b29a124387e09f5
soulreapersynch9 <soulreapersynch9@gmail.com>
2025-11-13 04:42:50 +0000

    Initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
 
 flutter {
